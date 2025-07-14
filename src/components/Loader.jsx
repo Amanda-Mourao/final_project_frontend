4fb1a6edee7818baa78accea6929c48f8e884479
parween92 <parweena46@gmail.com>
2025-07-14 17:40:42 +0200

    Loader IMG fixed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,11 @@ const Loading =
 export const Loader = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black flex items-center justify-center z-9999">
-      <img src={Loading} alt="Loading..." className="w-[300px] h-[300px]" />
+      <img
+        src={Loading}
+        alt="Loading..."
+        className="w-[300px] h-[300px] object-contain"
+      />
     </div>
   );
 };
