7bd7af79d1a37ac125455336603ece8f78ca88fc
Amanda Mourao <a-mourao@web.de>
2025-07-14 14:36:44 +0200

    backend render url

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL || "https://final-project-backend-3lca.onrender.com";
 
 // Zentrale Axios
 const api = axios.create({
